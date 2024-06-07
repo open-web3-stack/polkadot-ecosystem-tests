dd588058f64f3925d042d502ab5efd9fd00ffdcc
nsso <nxiiun@gmail.com>
2024-06-07 09:09:21 +0800

    fix dirname

--- packages/networks/src/chains/acala-next.ts
+++ packages/networks/src/chains/acala-next.ts
@@ -1,6 +1,11 @@
 import { acala, karura } from './acala.js'
 
-import path from 'path'
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+
+// Convert the URL to a path and get the directory name
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const acalaNext = acala.extend(() => ({
   name: 'acalaNext',
