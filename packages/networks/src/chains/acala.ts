4944fabaad2f03316e31e589614b261fa7a549bf
nsso <nxiiun@gmail.com>
2024-06-10 08:04:31 +0800

    fix

--- packages/networks/src/chains/acala.ts
+++ packages/networks/src/chains/acala.ts
@@ -65,7 +65,7 @@ export const getInitStorages = (config: typeof custom.acala | typeof custom.karu
 
 export const acala = defineChain({
   name: 'acala',
-  endpoint: ['wss://acala-rpc.aca-api.network', 'wss://acala-rpc.dwellir.com'],
+  endpoint: 'https://acala-rpc.aca-api.network',
   paraId: 2000,
   custom: custom.acala,
   initStorages: getInitStorages(custom.acala),
@@ -73,11 +73,7 @@ export const acala = defineChain({
 
 export const karura = defineChain({
   name: 'karura',
-  endpoint: [
-    'wss://karura-rpc.aca-api.network',
-    'wss://rpc-karura.luckyfriday.io',
-    'wss://karura.api.onfinality.io/public-ws',
-  ],
+  endpoint: 'https://karura-rpc.aca-api.network',
   paraId: 2000,
   custom: custom.karura,
   initStorages: getInitStorages(custom.karura),
