a1c45c8f78763bfb35895c6ed243ffe3d2fc811b
Bryan Chen <xlchen1291@gmail.com>
2024-09-30 17:10:05 +1300

    add coretime tests

---
+++ packages/networks/src/chains/coretime.ts
@@ -0,0 +1,33 @@
+import { defaultAccounts } from '../defaultAccounts.js'
+import { defineChain } from '../defineChain.js'
+
+const custom = {
+  coretimePolkadot: {
+    dot: { Concrete: { parents: 1, interior: 'Here' } },
+  },
+  coretimeKusama: {
+    ksm: { Concrete: { parents: 1, interior: 'Here' } },
+  },
+}
+
+const getInitStorages = (_config: typeof custom.coretimePolkadot | typeof custom.coretimeKusama) => ({
+  System: {
+    account: [[[defaultAccounts.alice.address], { providers: 1, data: { free: 1000e10 } }]],
+  },
+})
+
+export const coretimePolkadot = defineChain({
+  name: 'coretimePolkadot',
+  endpoint: 'wss://polkadot-coretime-rpc.polkadot.io',
+  paraId: 1005,
+  custom: custom.coretimePolkadot,
+  initStorages: getInitStorages(custom.coretimePolkadot),
+})
+
+export const coretimeKusama = defineChain({
+  name: 'coretimeKusama',
+  endpoint: 'wss://kusama-coretime-rpc.polkadot.io',
+  paraId: 1005,
+  custom: custom.coretimeKusama,
+  initStorages: getInitStorages(custom.coretimeKusama),
+})
---
+++ packages/shared/src/xcm/runXcmPalletUp.ts
@@ -0,0 +1,55 @@
+import { type KeyringPair } from '@polkadot/keyring/types'
+import { it } from 'vitest'
+import { sendTransaction } from '@acala-network/chopsticks-testing'
+
+import { Client, defaultAccounts } from '@e2e-test/networks'
+import { GetBalance, Tx } from './types.js'
+import { check, checkEvents, checkSystemEvents, checkUmp } from '../helpers/index.js'
+
+export const runXcmPalletUp = (
+  name: string,
+  setup: () => Promise<{
+    fromChain: Client
+    toChain: Client
+    tx: Tx
+    balance: GetBalance
+
+    fromAccount?: KeyringPair
+    toAccount?: KeyringPair
+    precision?: number
+  }>,
+  options: { only?: boolean } = {},
+) => {
+  const itfn = options.only ? it.only : it
+  itfn(
+    name,
+    async () => {
+      const {
+        fromChain,
+        toChain,
+        tx,
+        balance,
+        fromAccount = defaultAccounts.alice,
+        toAccount = defaultAccounts.bob,
+        precision = 3,
+      } = await setup()
+      const tx0 = await sendTransaction(tx(fromChain, toAccount.addressRaw).signAsync(fromAccount))
+
+      await fromChain.chain.newBlock()
+
+      await check(balance(fromChain, fromAccount.address))
+        .redact({ number: precision })
+        .toMatchSnapshot('balance on from chain')
+      await checkEvents(tx0, 'polkadotXcm', 'xcmPallet').redact({ number: precision }).toMatchSnapshot('tx events')
+      await checkUmp(fromChain).toMatchSnapshot('from chain ump messages')
+
+      await toChain.chain.newBlock()
+
+      await check(toChain.api.query.system.account(toAccount.address))
+        .redact({ number: precision })
+        .toMatchSnapshot('balance on to chain')
+      await checkSystemEvents(toChain, 'ump', 'messageQueue').toMatchSnapshot('to chain ump events')
+    },
+    240000,
+  )
+}
