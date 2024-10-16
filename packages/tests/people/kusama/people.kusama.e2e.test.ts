9e03133f0dfa0571a02b90f0edacb268e267365d
Alexandre Baldé <alexandre.balde@parity.io>
2024-10-16 23:48:03 +0100

    Add method to (sudo) execute calls in parachain
    This is required to test pallet extrinsics that require certain origins
    to be executed e.g. the identity pallet's `addRegistrar` function.
    
    The original source for the idea can be found in https://hackmd.io/@xlc/H11BX5BLa#Dispatch-Call

--- packages/tests/people/kusama/people.kusama.e2e.test.ts
+++ packages/tests/people/kusama/people.kusama.e2e.test.ts
@@ -4,6 +4,6 @@ import { peopleKusama } from '@e2e-test/networks/chains'
 
 import { setIdentityThenRequestAndProvideJudgement } from '../shared.js'
 
-describe('Polkadot People chain: setting on-chain identity and requesting judgement should work', async () => {
+describe('Kusama People chain: setting on-chain identity and requesting judgement should work', async () => {
   await setIdentityThenRequestAndProvideJudgement(peopleKusama)
 })
--- packages/tests/people/polkadot/people.polkadot.e2e.test.ts
+++ packages/tests/people/polkadot/people.polkadot.e2e.test.ts
@@ -1,9 +1,95 @@
 import { describe } from 'vitest'
 
-import { peoplePolkadot } from '@e2e-test/networks/chains'
-
 import { setIdentityThenRequestAndProvideJudgement } from '../shared.js'
+
+import { defaultAccounts } from '@e2e-test/networks'
+import { peoplePolkadot, polkadot } from '@e2e-test/networks/chains'
+import { setupNetworks } from '@e2e-test/shared'
 
 describe('Polkadot People chain: setting on-chain identity and requesting judgement should work', async () => {
   await setIdentityThenRequestAndProvideJudgement(peoplePolkadot)
+})
+
+describe('Adding a registrar as root from the relay chain works', async () => {
+  const [polkadotClient, peopleClient] = await setupNetworks(polkadot, peoplePolkadot)
+
+  const addRegistrarTx = peopleClient.api.tx.identity.addRegistrar(defaultAccounts.dave.address)
+  const encodedPeopleChainCalldata = addRegistrarTx.method.toHex()
+
+  const dest = {
+    V4: {
+      parents: 0,
+      interior: {
+        X1: [
+          {
+            Parachain: 1004,
+          },
+        ],
+      },
+    },
+  }
+
+  const message = {
+    V4: [
+      {
+        UnpaidExecution: {
+          weightLimit: 'Unlimited',
+          checkOrigin: null,
+        },
+      },
+      {
+        Transact: {
+          call: {
+            encoded: encodedPeopleChainCalldata,
+          },
+          originKind: 'SuperUser',
+          requireWeightAtMost: {
+            proofSize: '3000',
+            refTime: '1000000000',
+          },
+        },
+      },
+    ],
+  }
+
+  const addRegistrarXcmTx = polkadotClient.api.tx.xcmPallet.send(dest, message)
+
+  const encodedRelayCallData = addRegistrarXcmTx.method.toHex()
+
+  // Get the current block number, to be able to inform the relay chain's scheduler of the
+  // appropriate block to inject this call in.
+  const number = (await polkadotClient.api.rpc.chain.getHeader()).number.toNumber()
+
+  await polkadotClient.api.rpc('dev_setStorage', {
+    scheduler: {
+      agenda: [
+        [
+          [number + 1],
+          [
+            {
+              call: {
+                Inline: encodedRelayCallData,
+              },
+              origin: {
+                system: 'Root',
+              },
+            },
+          ],
+        ],
+      ],
+    },
+  })
+
+  // Create a new block in the relay chain so that the above XCM call can be executed in the
+  // parachain
+  await polkadotClient.chain.newBlock()
+
+  // Also advance a block in the parachain - otherwise, the above call's effect would not be visible.
+  await peopleClient.chain.newBlock()
+
+  const registrarsAfterParaBlock = await peopleClient.api.query.identity.registrars()
+
+  registrarsAfterParaBlock.map((registrar) => {
+    console.log(registrar.toHuman())
+  })
 })
