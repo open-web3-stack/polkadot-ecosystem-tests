79a0febc61610b0b78402c1a5c235fc32692beac
Alexandre Baldé <alexandre.balde@parity.io>
2024-10-17 15:41:07 +0100

    Add e2e test to identity cancellation/clearing

--- packages/tests/people/polkadot/people.polkadot.e2e.test.ts
+++ packages/tests/people/polkadot/people.polkadot.e2e.test.ts
@@ -2,12 +2,21 @@ import { describe } from 'vitest'
 
 import { peoplePolkadot, polkadot } from '@e2e-test/networks/chains'
 
-import { addRegistrarViaRelayAsRoot, setIdentityThenRequestAndProvideJudgement } from '../shared.js'
+import {
+  addRegistrarViaRelayAsRoot,
+  setIdentityThenRequesThenCancelThenClear,
+  setIdentityThenRequestAndProvideJudgement,
+} from '../shared.js'
 
-describe('Polkadot people chain: setting on-chain identity and requesting judgement should work', async () => {
+/* describe('Polkadot people chain: setting on-chain identity and requesting judgement should work', async () => {
   await setIdentityThenRequestAndProvideJudgement(peoplePolkadot)
+}) */
+
+describe('Polkadot people chain: setting on-chain identity, requesting judgement, cancelling the request and then clearing the identity should work', async () => {
+  await setIdentityThenRequesThenCancelThenClear(peoplePolkadot)
 })
 
-describe('Polkadot people chain: Adding a registrar as root from the relay chain works', async () => {
+/* describe('Polkadot people chain: Adding a registrar as root from the relay chain works', async () => {
   await addRegistrarViaRelayAsRoot(polkadot, peoplePolkadot)
 })
+ */
--- packages/tests/people/shared.ts
+++ packages/tests/people/shared.ts
@@ -43,7 +43,7 @@ export async function setIdentityThenRequestAndProvideJudgement<
   const txApi = peopleClient.api.tx
 
   /**
-   * Set Alice's on-chain identity
+   * Set Bob's on-chain identity
    */
 
   const setIdTx = txApi.identity.setIdentity(identity)
@@ -59,13 +59,14 @@ export async function setIdentityThenRequestAndProvideJudgement<
   console.log(identityInfo.toHuman())
   assert(
     registrationInfo.judgements.isEmpty,
-    'Error: immediately after setIdentity, there should be no judgments on the identity.',
+    'Error: immediately after `setIdentity`, there should be no judgments on the identity.',
   )
 
   /**
    * Request a judgement on identity that was just set
    */
 
+  // Recall that, in the people chain's test storage, Alice is the 0th registrar.
   const reqJudgTx = txApi.identity.requestJudgement(0, 1)
   await reqJudgTx.signAndSend(defaultAccounts.bob)
 
@@ -82,7 +83,7 @@ export async function setIdentityThenRequestAndProvideJudgement<
   const provisionalIdentityInfo: PalletIdentityLegacyIdentityInfo = provisionalRegistrationInfo.info
   assert(provisionalRegistrationInfo.judgements.length === 1, 'There should only be 1 judgement after requesting it.')
 
-  // Freely indexing into the `Vec` here, as it *should? have 1 element.
+  // Freely indexing into the `Vec` here, as it *should* have 1 element.
   const provisionalJudgement: ITuple<[u32, PalletIdentityJudgement]> = provisionalRegistrationInfo.judgements[0]
   assert(identityInfo.eq(provisionalIdentityInfo), 'Identity information changed after judgement request')
   assert(provisionalJudgement[0].eq('0'), 'Alice, to whom a request was made, should be the 0th registrar')
@@ -122,6 +123,111 @@ export async function setIdentityThenRequestAndProvideJudgement<
   assert(identityInfo.eq(provisionalIdentityInfo), 'Identity information changed after judgement request')
   assert(judgement[0].eq('0'), 'Alice, from whom a judgement was received, should be the 0th registrar')
   assert(judgement[1].isReasonable, 'The judgement immediately after _this_ judgement should be "Reasonable"')
+}
+
+/**
+ * Test to the process of
+ * 1. setting an identity,
+ * 2. requesting a judgement,
+ * 3. cancellingi the previous request, and
+ * 4. clearing the identity
+ *
+ * @param peopleChain People parachain where the entire process is run.
+ */
+export async function setIdentityThenRequesThenCancelThenClear<
+  TCustom extends Record<string, unknown> | undefined,
+  TInitStorages extends Record<string, Record<string, any>> | undefined,
+>(peopleChain: Chain<TCustom, TInitStorages>) {
+  const [peopleClient] = await setupNetworks(peopleChain)
+
+  const identity = {
+    email: { raw: 'test_address@test.io' },
+    legal: { raw: 'FirstName LastName' },
+    matrix: { raw: '@test:test_server.io' },
+    twitter: { Raw: '@test_twitter' },
+    github: { Raw: 'test_github' },
+    discord: { Raw: 'test_discord' },
+    web: { Raw: 'http://test.te/me' },
+    image: { raw: 'test' },
+    display: { raw: 'Test Display' },
+    pgpFingerprint: 'a1b2c3d4e5f6g7h8i9j1',
+  }
+
+  const querier = peopleClient.api.query
+  const txApi = peopleClient.api.tx
+
+  /**
+   * Set Bob's on-chain identity
+   */
+
+  const setIdTx = txApi.identity.setIdentity(identity)
+  await setIdTx.signAndSend(defaultAccounts.bob)
+
+  await peopleClient.chain.newBlock()
+
+  const identityInfoReply = await querier.identity.identityOf(defaultAccounts.bob.address)
+  assert(identityInfoReply.isSome, 'Failed to query set identity')
+  const registrationInfo: PalletIdentityRegistration = identityInfoReply.unwrap()[0]
+
+  assert(
+    registrationInfo.judgements.isEmpty,
+    'Error: immediately after `setIdentity`, there should be no judgments on the identity.',
+  )
+
+  /**
+   * Request a judgement on identity that was just set
+   */
+
+  const reqJudgTx = txApi.identity.requestJudgement(0, 1)
+  await reqJudgTx.signAndSend(defaultAccounts.bob)
+
+  await peopleClient.chain.newBlock()
+
+  /**
+   * Check post-request identity state
+   */
+
+  const provisionalIdentityInfoReply = await querier.identity.identityOf(defaultAccounts.bob.address)
+  assert(provisionalIdentityInfoReply.isSome, 'Failed to query identity after judgement')
+  const provisionalRegistrationInfo = provisionalIdentityInfoReply.unwrap()[0]
+
+  assert(provisionalRegistrationInfo.judgements.length === 1, 'There should only be 1 judgement after requesting it.')
+
+  // Freely indexing into the `Vec` here, as it *should* have 1 element.
+  const provisionalJudgement: ITuple<[u32, PalletIdentityJudgement]> = provisionalRegistrationInfo.judgements[0]
+  assert(provisionalJudgement[0].eq('0'), 'Alice, to whom a request was made, should be the 0th registrar')
+  assert(provisionalJudgement[1].isFeePaid, 'The judgement immediately after a request should be "FeePaid"')
+  assert(provisionalJudgement[1].asFeePaid.eq(1), "Alice's registrar fee should be set to `1`")
+
+  /**
+   * Cancel the previous judgement request
+   */
+
+  const cancelJudgTx = txApi.identity.cancelRequest(0)
+  await cancelJudgTx.signAndSend(defaultAccounts.bob)
+
+  await peopleClient.chain.newBlock()
+
+  const newIdentityInfoReply = await querier.identity.identityOf(defaultAccounts.bob.address)
+  assert(newIdentityInfoReply.isSome, 'Failed to query set identity')
+  const newRegistrationInfo: PalletIdentityRegistration = newIdentityInfoReply.unwrap()[0]
+
+  assert(
+    newRegistrationInfo.judgements.isEmpty,
+    'Error: immediately after `cancelRequest`, there should be no judgments on the identity.',
+  )
+
+  /**
+   * Clear the tentatively set identity
+   */
+
+  const clearIdTx = txApi.identity.clearIdentity()
+  await clearIdTx.signAndSend(defaultAccounts.bob)
+
+  await peopleClient.chain.newBlock()
+
+  const identityInfoNullReply = await querier.identity.identityOf(defaultAccounts.bob.address)
+  assert(identityInfoNullReply.isNone, "Bob's identity should be empty after it is cleared")
 }
 
 /**
