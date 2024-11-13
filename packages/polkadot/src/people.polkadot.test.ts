7200247462e44fd194fe2847ffe2e135734a4b51
Andrii <ndk@parity.io>
2024-11-13 16:44:44 +0100

    Added tests for xcm messages (#95)
    * first xcm test with send. snapshot shows an error...
    
    * changed xcm version to 4, improved the message, error status changed from Unsupported to ProcessingFailed

--- packages/polkadot/src/people.polkadot.test.ts
+++ packages/polkadot/src/people.polkadot.test.ts
@@ -31,4 +31,70 @@ describe('polkadot & peoplePolkadot', async () => {
       tx: tx.xcmPallet.teleportAssetsV3(peopleDOT, 1e12, tx.xcmPallet.relaychainV4),
     }
   })
+
+  runXcmPalletUp('Send dummy XCM', async () => {
+    return {
+      fromChain: peopleClient,
+      toChain: polkadotClient,
+      balance: query.balances,
+      toAccount: defaultAccounts.charlie,
+      tx: tx.xcmPallet.sendXCM(
+        {
+          V4: {
+            parents: 1,
+            interior: 'Here',
+          },
+        },
+        {
+          V4: [
+            {
+              WithdrawAsset: [
+                {
+                  id: {
+                    parents: 0,
+                    interior: 'Here',
+                  },
+                  fun: { Fungible: 1e12 },
+                },
+              ],
+            },
+            {
+              BuyExecution: {
+                fees: {
+                  id: {
+                    parents: 0,
+                    interior: 'Here',
+                  },
+                  fun: {
+                    fungible: 1_000_000_000n,
+                  },
+                },
+                weight_limit: 'unlimited',
+              },
+            },
+            {
+              DepositAsset: {
+                assets: {
+                  wild: 'all',
+                },
+                beneficiary: {
+                  parents: 0,
+                  interior: {
+                    x1: [
+                      {
+                        accountId32: {
+                          id: '0x5e639b43e0052c47447dac87d6fd2b6ec50bdd4d0f614e4299c665249bbd09d9',
+                          network: null,
+                        },
+                      },
+                    ],
+                  },
+                },
+              },
+            },
+          ],
+        },
+      ),
+    }
+  })
 })
