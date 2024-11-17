38ce3bc645015d96e0f16614c0315c4c33b0aefb
Alexandre Baldé <alexandre.balde@parity.io>
2024-11-17 01:44:20 +0000

    Draft governance test with PAPI (WIP)

--- packages/shared/src/governance.ts
+++ packages/shared/src/governance.ts
@@ -1,7 +1,12 @@
-import { Chain, defaultAccounts } from '@e2e-test/networks'
 import { assert, describe, test } from 'vitest'
+
+import { Chain, defaultAccounts } from '@e2e-test/networks'
 import { check, checkEvents } from '@e2e-test/shared/helpers'
 import { setupNetworks } from '@e2e-test/shared'
+
+import { sendTransaction } from '@acala-network/chopsticks-testing'
+
+import { BN } from 'bn.js'
 
 // `dot` is the name we gave to `npx papi add`
 import {
@@ -15,16 +20,19 @@ import {
 import { createClient } from 'polkadot-api'
 // import from "polkadot-api/ws-provider/node"
 // if you are running in a NodeJS environment
+import { DEV_PHRASE, entropyToMiniSecret, mnemonicToEntropy } from '@polkadot-labs/hdkd-helpers'
 import { getPolkadotSigner } from 'polkadot-api/signer'
 import { getWsProvider } from 'polkadot-api/ws-provider/node'
+import { sr25519CreateDerive } from '@polkadot-labs/hdkd'
 import { withPolkadotSdkCompat } from 'polkadot-api/polkadot-sdk-compat'
 
-import { DEV_PHRASE, entropyToMiniSecret, mnemonicToEntropy } from '@polkadot-labs/hdkd-helpers'
 import { Keyring } from '@polkadot/api'
-import { Option } from '@polkadot/types'
-import { PalletReferendaReferendumInfoConvictionVotingTally } from '@polkadot/types/lookup'
-import { sendTransaction } from '@acala-network/chopsticks-testing'
-import { sr25519CreateDerive } from '@polkadot-labs/hdkd'
+import { Option, u32 } from '@polkadot/types'
+import {
+  PalletReferendaReferendumInfoConvictionVotingTally,
+  PalletReferendaReferendumStatusConvictionVotingTally,
+} from '@polkadot/types/lookup'
+import { encodeAddress } from '@polkadot/util-crypto'
 
 const entropy = mnemonicToEntropy(DEV_PHRASE)
 const miniSecret = entropyToMiniSecret(entropy)
@@ -60,19 +68,26 @@ export async function submitReferendumThenCancel<
   /**
    * Setup relay and parachain clients
    */
-
   const [relayClient] = await setupNetworks(relayChain)
 
-  const encodedProposal = relayClient.api.tx.treasury.spendLocal(1e10, defaultAccounts.bob.address).method.toHex()
-
+  /*
   const preimageTx = relayClient.api.tx.preimage.notePreimage(encodedProposal)
-  //const preImageEvents = await sendTransaction(preimageTx.signAsync(defaultAccounts.alice))
+  const preImageEvents = await sendTransaction(preimageTx.signAsync(defaultAccounts.alice))
 
   await relayClient.dev.newBlock()
 
-  //await checkEvents(preImageEvents, 'preimage').toMatchSnapshot('note preimage events')
+  await checkEvents(preImageEvents, 'preimage').toMatchSnapshot('note preimage events')
+  */
 
-  const referendumNum = await relayClient.api.query.referenda.referendumCount()
+  /**
+   * Get current referendum count i.e. the next referendum's index
+   */
+  const referendumIndex = await relayClient.api.query.referenda.referendumCount()
+
+  /**
+   * Submit a new referendum
+   */
+  const encodedProposal = relayClient.api.tx.treasury.spendLocal(1e10, defaultAccounts.bob.address).method.toHex()
 
   const submitReferendumTx = relayClient.api.tx.referenda.submit(
     {
@@ -89,18 +104,56 @@ export async function submitReferendumThenCancel<
 
   await relayClient.dev.newBlock()
 
-  await checkEvents(submitReferendumEvents, 'referenda').redact({}).toMatchSnapshot('referendum submission events')
+  // Fields to be removed, check comment below.
+  let unwantedFields: RegExp = new RegExp('index')
+  await checkEvents(submitReferendumEvents, 'referenda')
+    .redact({ removeKeys: unwantedFields })
+    .toMatchSnapshot('referendum submission events')
+
+  /**
+   * Check the created referendum's data
+   */
 
   const referendumDataOpt: Option<PalletReferendaReferendumInfoConvictionVotingTally> =
-    await relayClient.api.query.referenda.referendumInfoFor(referendumNum)
+    await relayClient.api.query.referenda.referendumInfoFor(referendumIndex)
   assert(referendumDataOpt, "submitted referendum's data cannot be `None`")
   const referendumData: PalletReferendaReferendumInfoConvictionVotingTally = referendumDataOpt.unwrap()
-
-  const unwantedFields: RegExp = new RegExp('alarm')
-
+  // These fields must be excised from the queried referendum data before being put in the test
+  // snapshot.
+  // These fields contain epoch-sensitive data, which will cause spurious test failures
+  // periodically.
+  unwantedFields = new RegExp('asdasdsadasdasd') //alarm|submitted")
   await check(referendumData).redact({ removeKeys: unwantedFields }).toMatchSnapshot('referendum info')
 
-  await relayClient.pause()
+  assert(referendumData.isOngoing)
+  const ongoingRef: PalletReferendaReferendumStatusConvictionVotingTally = referendumData.asOngoing
+
+  assert(ongoingRef.alarm.isSome)
+  const undecidingTimeoutAlarm = ongoingRef.alarm.unwrap()[0]
+  const blocksUntilAlarm = undecidingTimeoutAlarm.sub(ongoingRef.submitted)
+  assert(blocksUntilAlarm.eq(relayClient.api.consts.referenda.undecidingTimeout))
+
+  assert(ongoingRef.enactment.isAfter)
+  await check(ongoingRef.enactment.asAfter).toMatchObject(1)
+
+  const referendaTracks = relayClient.api.consts.referenda.tracks
+  const smallTipper = referendaTracks.find((track) => track[1].name.eq('small_tipper'))!
+  assert(ongoingRef.track.eq(smallTipper[0]))
+  await check(ongoingRef.origin).toMatchObject({
+    origins: 'SmallTipper',
+  })
+  assert(ongoingRef.deciding.isNone)
+  assert(ongoingRef.decisionDeposit.isNone)
+
+  assert(ongoingRef.submissionDeposit.who.eq(encodeAddress(defaultAccounts.alice.address, addressEncoding)))
+  assert(ongoingRef.submissionDeposit.amount.eq(relayClient.api.consts.referenda.submissionDeposit))
+
+  // Check that voting data is empty
+  await check(ongoingRef.tally).toMatchObject({
+    ayes: 0,
+    nays: 0,
+    support: 0,
+  })
 
   return
 
