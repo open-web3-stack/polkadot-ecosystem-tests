98669c2e74d51002a2314129f9b10f46a647c3bd
Alexandre Baldé <alexandre.balde@parity.io>
2025-02-28 00:51:44 +0100

    Correct type errors in default account objects

--- packages/networks/src/defaultAccounts.ts
+++ packages/networks/src/defaultAccounts.ts
@@ -1,10 +1,24 @@
 import { testingPairs } from '@acala-network/chopsticks-testing'
+import type { KeyringInstance, KeyringPair } from '@polkadot/keyring/types'
 
 import { cryptoWaitReady } from '@polkadot/util-crypto'
 
 export type DefaultAccounts = ReturnType<typeof testingPairs>
 
-export const defaultAccounts = testingPairs()
+export const defaultAccounts: {
+  alice: KeyringPair
+  bob: KeyringPair
+  charlie: KeyringPair
+  dave: KeyringPair
+  eve: KeyringPair
+  alith: KeyringPair
+  baltathar: KeyringPair
+  charleth: KeyringPair
+  dorothy: KeyringPair
+  ethan: KeyringPair
+  keyring: any
+  keyringEth: KeyringInstance
+} = testingPairs()
 
 /**
  * Sr25519 keyring pairs for use in tests.
@@ -12,4 +26,17 @@ export const defaultAccounts = testingPairs()
  * These are preferrable over Ed25519 because PJS offers Sr25519 development keypairs when used in conjunction with
  * `chopsticks`, which helps debugging tests when `pause()`ing.
  */
-export const defaultAccountsSr25199 = await cryptoWaitReady().then(() => testingPairs('sr25519'))
+export const defaultAccountsSr25199: {
+  alice: KeyringPair
+  bob: KeyringPair
+  charlie: KeyringPair
+  dave: KeyringPair
+  eve: KeyringPair
+  alith: KeyringPair
+  baltathar: KeyringPair
+  charleth: KeyringPair
+  dorothy: KeyringPair
+  ethan: KeyringPair
+  keyring: any
+  keyringEth: KeyringInstance
+} = await cryptoWaitReady().then(() => testingPairs('sr25519'))
