242614fbb2951db297b5f8e20946c498d1316c7c
Karol Kokoszka <karol@parity.io>
2025-03-04 09:46:05 +0100

    [E2E Collectives] Introduce notifications and correct test naming

--- packages/polkadot/src/collectivesPolkadot.polkadot.test.ts
+++ packages/polkadot/src/collectivesPolkadot.polkadot.test.ts
@@ -2,4 +2,4 @@ import { collectivesPolkadot, polkadot } from '@e2e-test/networks/chains'
 
 import { collectivesChainE2ETests } from '@e2e-test/shared'
 
-collectivesChainE2ETests(polkadot, collectivesPolkadot, { testSuiteName: 'Polkadot Collectives' })
+collectivesChainE2ETests(polkadot, collectivesPolkadot, { testSuiteName: 'collectives & polkadot' })
