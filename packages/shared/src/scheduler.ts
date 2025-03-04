5e186f5bbf05023913716bbb2d8da9acb04df15d
Alexandre Baldé <alexandre.balde@parity.io>
2025-03-04 19:15:00 +0000

    Test scheduling named task with bad origin

--- packages/shared/src/scheduler.ts
+++ packages/shared/src/scheduler.ts
@@ -2,26 +2,20 @@ import { assert, describe, test } from 'vitest'
 
 import { type Chain, defaultAccountsSr25199 } from '@e2e-test/networks'
 import { setupNetworks } from '@e2e-test/shared'
+import CryptoJS from 'crypto-js'
 import { checkSystemEvents } from './helpers/index.js'
 
 import { sendTransaction } from '@acala-network/chopsticks-testing'
+import type { SubmittableExtrinsic } from '@polkadot/api/types'
+import type { ISubmittableResult } from '@polkadot/types/types'
 
-/**
- * Test the process of scheduling a call with a bad origin, and check that it fails.
- */
-export async function scheduleBadOriginTest<
-  TCustom extends Record<string, unknown> | undefined,
-  TInitStorages extends Record<string, Record<string, any>> | undefined,
->(chain: Chain<TCustom, TInitStorages>, addressEncoding: number) {
-  const [client] = await setupNetworks(chain)
+/// -------
+/// Helpers
+/// -------
 
+export async function badOriginHelper(client: any, scheduleTx: SubmittableExtrinsic<'promise', ISubmittableResult>) {
   const alice = defaultAccountsSr25199.alice
 
-  const currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
-
-  const call = client.api.tx.system.remark('test').method.toHex()
-
-  const scheduleTx = client.api.tx.scheduler.schedule(currBlockNumber, null, 0, call)
   await sendTransaction(scheduleTx.signAsync(alice))
 
   await client.dev.newBlock()
@@ -44,13 +38,62 @@ export async function scheduleBadOriginTest<
   assert(dispatchError.isBadOrigin)
 }
 
+/// -------
+/// -------
+/// -------
+
+// const currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
+// const call = client.api.tx.system.remark('test').method.toHex()
+// client.api.tx.scheduler.schedule(currBlockNumber, null, 0, call)
+
+/**
+ * Test the process of scheduling a call with a bad origin, and check that it fails.
+ */
+export async function scheduleBadOriginTest<
+  TCustom extends Record<string, unknown> | undefined,
+  TInitStorages extends Record<string, Record<string, any>> | undefined,
+>(chain: Chain<TCustom, TInitStorages>) {
+  const [client] = await setupNetworks(chain)
+
+  const currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
+  const call = client.api.tx.system.remark('test').method.toHex()
+  const scheduleTx = client.api.tx.scheduler.schedule(currBlockNumber, null, 0, call)
+
+  await badOriginHelper(client, scheduleTx)
+}
+
+/**
+ * Test the process of scheduling a named call with a bad origin, and check that it fails.
+ */
+export async function scheduleNamedBadOriginTest<
+  TCustom extends Record<string, unknown> | undefined,
+  TInitStorages extends Record<string, Record<string, any>> | undefined,
+>(chain: Chain<TCustom, TInitStorages>) {
+  const [client] = await setupNetworks(chain)
+
+  const currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
+  const call = client.api.tx.system.remark('test').method.toHex()
+
+  const hash = CryptoJS.SHA256('task_id')
+  const buf = Buffer.from(hash.toString(CryptoJS.enc.Hex), 'hex')
+  const taskId = new Uint8Array(buf)
+
+  const scheduleTx = client.api.tx.scheduler.scheduleNamed(taskId, currBlockNumber, null, 0, call)
+
+  await badOriginHelper(client, scheduleTx)
+}
+
 export function schedulerE2ETests<
   TCustom extends Record<string, unknown> | undefined,
   TInitStoragesRelay extends Record<string, Record<string, any>> | undefined,
 >(chain: Chain<TCustom, TInitStoragesRelay>, testConfig: { testSuiteName: string; addressEncoding: number }) {
   describe(testConfig.testSuiteName, () => {
     test('schedule task with wrong origin', async () => {
-      await scheduleBadOriginTest(chain, testConfig.addressEncoding)
+      await scheduleBadOriginTest(chain)
+    })
+
+    test('schedule named task with wrong origin', async () => {
+      await scheduleNamedBadOriginTest(chain)
     })
   })
 }
