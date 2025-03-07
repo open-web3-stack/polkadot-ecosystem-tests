94adf18b3ff4246bb975ce76f3a918ae1de7f4b7
Alexandre Baldé <alexandre.balde@parity.io>
2025-03-07 14:01:52 +0000

    Test scheduling of permanently overweight task
    This reworks the previous WIP commit to create a new test.

--- packages/shared/src/scheduler.ts
+++ packages/shared/src/scheduler.ts
@@ -7,7 +7,6 @@ import { check, checkSystemEvents, scheduleCallWithOrigin } from './helpers/inde
 
 import { sendTransaction } from '@acala-network/chopsticks-testing'
 import type { SubmittableExtrinsic } from '@polkadot/api/types'
-import type { Option } from '@polkadot/types'
 import type { PalletSchedulerScheduled } from '@polkadot/types/lookup'
 import type { ISubmittableResult } from '@polkadot/types/types'
 
@@ -180,58 +179,76 @@ export async function cancelNamedScheduledTaskBadOriginTest<
   await badOriginHelper(client, cancelTx, 'events when cancelling named task with insufficient origin')
 }
 
-export async function experiment<
+/**
+ * Test the process of
+ *
+ * 1. creating a call requiring a `Root` origin: an update to total issuance
+ * 2. artificially manipulating that call's weight to the per-block weight limit allotted to scheduled calls
+ * 3. scheduling the call
+ * 4. checking that the call was not executed
+ * 5. checking that it remains in the agenda for the original block it was scheduled in
+ * @param chain
+ *
+ */
+export async function scheduledOverweightCallFails<
   TCustom extends Record<string, unknown> | undefined,
   TInitStorages extends Record<string, Record<string, any>> | undefined,
 >(chain: Chain<TCustom, TInitStorages>) {
   const [client] = await setupNetworks(chain)
 
+  // Call whose weight will be artifically inflated
   const adjustIssuanceTx = client.api.tx.balances.forceAdjustTotalIssuance('Increase', 1)
 
-  // Call whose weight will be artifically inflated
-  const remarkTx = client.api.tx.system.remark('test')
-  const remarkTxSigned = await remarkTx.signAsync(defaultAccountsSr25519.alice)
   // Network's maximum allowed weight for scheduled calls.
-  const { refTime, proofSize } = client.api.consts.scheduler.maximumWeight
+  const maxWeight = client.api.consts.scheduler.maximumWeight
 
-  const maxWeight = { refTime: refTime.unwrap().toBn(), proofSize: proofSize.unwrap().toBn() }
-  maxWeight.refTime = maxWeight.refTime.divn(10)
-  maxWeight.proofSize = maxWeight.proofSize.divn(10)
+  const withWeightTx = client.api.tx.utility.withWeight(adjustIssuanceTx, maxWeight)
 
-  //const withWeightTx = client.api.tx.utility.withWeight(remarkTxSigned, maxWeight)
-
-  console.log((await client.api.call.transactionPaymentCallApi.queryCallInfo(remarkTxSigned, 0)).toHuman())
-
-  const currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
-  const scheduleTx = client.api.tx.scheduler.schedule(currBlockNumber + 2, null, 0, adjustIssuanceTx)
+  let currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
+  const scheduleTx = client.api.tx.scheduler.schedule(currBlockNumber + 2, null, 0, withWeightTx)
 
   scheduleCallWithOrigin(client, scheduleTx.method.toHex(), { system: 'Root' })
 
-  await client.pause()
+  await client.dev.newBlock()
+
+  currBlockNumber += 1
+  let scheduled = await client.api.query.scheduler.agenda(currBlockNumber + 1)
+
+  assert(scheduled.length === 1)
+  const scheduledTask: PalletSchedulerScheduled = scheduled[0].unwrap()
+  const task = {
+    maybeId: null,
+    priority: 0,
+    call: { inline: withWeightTx.method.toHex() },
+    maybePeriodic: null,
+    origin: {
+      system: {
+        root: null,
+      },
+    },
+  }
+  await check(scheduledTask).toMatchObject(task)
+
+  // Get current total issuance
+  const totalIssuance = await client.api.query.balances.totalIssuance()
 
   await client.dev.newBlock()
 
-  await client.pause()
+  currBlockNumber += 1
 
-  let scheduled: Vec<Option<PalletSchedulerScheduled>> = null
-  for (let i = 0; i < 4; i++) {
-    scheduled = await client.api.query.scheduler.agenda(currBlockNumber + i)
-    console.log(scheduled.toHuman())
-  }
+  // Check that the call was not executed
+  const newTotalIssuance = await client.api.query.balances.totalIssuance()
+  assert(newTotalIssuance.eq(totalIssuance))
 
-  await client.dev.newBlock()
+  // Check that an event was emitted certifying the scheduled call as overweight
 
-  for (let i = 0; i < 4; i++) {
-    scheduled = await client.api.query.scheduler.agenda(currBlockNumber + i)
-    console.log(scheduled.toHuman())
-  }
+  await checkSystemEvents(client, 'scheduler').toMatchSnapshot('events when scheduling overweight task')
 
-  await client.dev.newBlock()
+  // Check that the call remains in the agenda for the original block it was scheduled in
+  scheduled = await client.api.query.scheduler.agenda(currBlockNumber)
+  assert(scheduled.length === 1)
 
-  for (let i = 0; i < 4; i++) {
-    scheduled = await client.api.query.scheduler.agenda(currBlockNumber + i)
-    console.log(scheduled.toHuman())
-  }
+  await check(scheduled[0].unwrap()).toMatchObject(task)
 }
 
 export function schedulerE2ETests<
@@ -255,8 +272,8 @@ export function schedulerE2ETests<
       await cancelNamedScheduledTaskBadOriginTest(chain)
     })
 
-    test('experiment', async () => {
-      await experiment(chain)
+    test('scheduling an overweight call is possible, but the call itself fails', async () => {
+      await scheduledOverweightCallFails(chain)
     })
   })
 }
