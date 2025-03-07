2300994b1d629a938d44678bb398a8871eee4288
Alexandre Baldé <alexandre.balde@parity.io>
2025-03-07 12:58:53 +0000

    [WIP] Test overweight task scheduling

--- packages/shared/src/scheduler.ts
+++ packages/shared/src/scheduler.ts
@@ -1,12 +1,14 @@
 import { assert, describe, test } from 'vitest'
 
-import { type Chain, defaultAccountsSr25199 } from '@e2e-test/networks'
+import { type Chain, defaultAccountsSr25519 } from '@e2e-test/networks'
 import { setupNetworks } from '@e2e-test/shared'
 import CryptoJS from 'crypto-js'
 import { check, checkSystemEvents, scheduleCallWithOrigin } from './helpers/index.js'
 
 import { sendTransaction } from '@acala-network/chopsticks-testing'
 import type { SubmittableExtrinsic } from '@polkadot/api/types'
+import type { Option } from '@polkadot/types'
+import type { PalletSchedulerScheduled } from '@polkadot/types/lookup'
 import type { ISubmittableResult } from '@polkadot/types/types'
 
 /// -------
@@ -18,7 +20,7 @@ export async function badOriginHelper(
   scheduleTx: SubmittableExtrinsic<'promise', ISubmittableResult>,
   snapshotDescription: string,
 ) {
-  const alice = defaultAccountsSr25199.alice
+  const alice = defaultAccountsSr25519.alice
 
   await sendTransaction(scheduleTx.signAsync(alice))
 
@@ -178,6 +180,60 @@ export async function cancelNamedScheduledTaskBadOriginTest<
   await badOriginHelper(client, cancelTx, 'events when cancelling named task with insufficient origin')
 }
 
+export async function experiment<
+  TCustom extends Record<string, unknown> | undefined,
+  TInitStorages extends Record<string, Record<string, any>> | undefined,
+>(chain: Chain<TCustom, TInitStorages>) {
+  const [client] = await setupNetworks(chain)
+
+  const adjustIssuanceTx = client.api.tx.balances.forceAdjustTotalIssuance('Increase', 1)
+
+  // Call whose weight will be artifically inflated
+  const remarkTx = client.api.tx.system.remark('test')
+  const remarkTxSigned = await remarkTx.signAsync(defaultAccountsSr25519.alice)
+  // Network's maximum allowed weight for scheduled calls.
+  const { refTime, proofSize } = client.api.consts.scheduler.maximumWeight
+
+  const maxWeight = { refTime: refTime.unwrap().toBn(), proofSize: proofSize.unwrap().toBn() }
+  maxWeight.refTime = maxWeight.refTime.divn(10)
+  maxWeight.proofSize = maxWeight.proofSize.divn(10)
+
+  //const withWeightTx = client.api.tx.utility.withWeight(remarkTxSigned, maxWeight)
+
+  console.log((await client.api.call.transactionPaymentCallApi.queryCallInfo(remarkTxSigned, 0)).toHuman())
+
+  const currBlockNumber = (await client.api.rpc.chain.getHeader()).number.toNumber()
+  const scheduleTx = client.api.tx.scheduler.schedule(currBlockNumber + 2, null, 0, adjustIssuanceTx)
+
+  scheduleCallWithOrigin(client, scheduleTx.method.toHex(), { system: 'Root' })
+
+  await client.pause()
+
+  await client.dev.newBlock()
+
+  await client.pause()
+
+  let scheduled: Vec<Option<PalletSchedulerScheduled>> = null
+  for (let i = 0; i < 4; i++) {
+    scheduled = await client.api.query.scheduler.agenda(currBlockNumber + i)
+    console.log(scheduled.toHuman())
+  }
+
+  await client.dev.newBlock()
+
+  for (let i = 0; i < 4; i++) {
+    scheduled = await client.api.query.scheduler.agenda(currBlockNumber + i)
+    console.log(scheduled.toHuman())
+  }
+
+  await client.dev.newBlock()
+
+  for (let i = 0; i < 4; i++) {
+    scheduled = await client.api.query.scheduler.agenda(currBlockNumber + i)
+    console.log(scheduled.toHuman())
+  }
+}
+
 export function schedulerE2ETests<
   TCustom extends Record<string, unknown> | undefined,
   TInitStoragesRelay extends Record<string, Record<string, any>> | undefined,
@@ -197,6 +253,10 @@ export function schedulerE2ETests<
 
     test('cancel named scheduled task with wrong origin', async () => {
       await cancelNamedScheduledTaskBadOriginTest(chain)
+    })
+
+    test('experiment', async () => {
+      await experiment(chain)
     })
   })
 }
