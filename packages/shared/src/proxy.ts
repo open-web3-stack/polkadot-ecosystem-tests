b09533bcd357e2ef20151450ae0dbe6c3ec143d1
Alexandre Baldé <alexandre.balde@parity.io>
2025-03-19 18:52:12 +0000

    Rework basic test to also check `remove_proxies`

--- packages/shared/src/proxy.ts
+++ packages/shared/src/proxy.ts
@@ -10,19 +10,29 @@ import type { ISubmittableResult } from '@polkadot/types/types'
 import { encodeAddress } from '@polkadot/util-crypto'
 import { assert, describe, test } from 'vitest'
 import { check, checkEvents } from './helpers/index.js'
+
 /**
- * Test to the process of adding a proxy to an account.
+ * Delay parameter for proxy tests.
+ */
+const PROXY_DELAY = 5
+
+/**
+ * Test to the process of adding and removing proxies to another account.
  *
  * 1. creates proxies of every type for an account
+ *     - these proxies have a delay of 0
  * 2. checks that the proxies exist
  * 3. removes every previously created proxy
- * 4. checks that the proxies no longerexist
- * @param client
+ * 4. checks that the proxies no longer exist
+ * 5. creates proxies of every type for the same account, this time with a delay
+ * 6. checks that the proxies exist
+ * 7. removes every previously created proxy with `remove_proxies`
+ * 8. checks that the proxies no longer exist
  */
-export async function addProxyTest<
+export async function addRemoveProxyTest<
   TCustom extends Record<string, unknown> | undefined,
   TInitStorages extends Record<string, Record<string, any>> | undefined,
->(client: Client<TCustom, TInitStorages>, addressEncoding: number, proxyTypes: Record<string, number>) {
+>(client: Client<TCustom, TInitStorages>, addressEncoding: number, proxyTypes: Record<string, number>, delay: number) {
   const alice = defaultAccountsSr25519.alice
   const kr = defaultAccountsSr25519.keyring
 
@@ -51,14 +61,14 @@ export async function addProxyTest<
 
   // Check created proxies
 
-  const proxyData = await client.api.query.proxy.proxies(alice.address)
-  const proxies: Vec<PalletProxyProxyDefinition> = proxyData[0]
+  let proxyData = await client.api.query.proxy.proxies(alice.address)
+  let proxies: Vec<PalletProxyProxyDefinition> = proxyData[0]
   assert(proxies.length === Object.keys(proxyTypes).length)
 
-  const proxyDeposit = proxyData[1]
-  const proxyDepositBase = client.api.consts.proxy.proxyDepositBase
-  const proxyDepositFactor = client.api.consts.proxy.proxyDepositFactor
-  const proxyDepositTotal = proxyDepositBase.add(proxyDepositFactor.muln(Object.keys(proxyTypes).length))
+  let proxyDeposit = proxyData[1]
+  let proxyDepositBase = client.api.consts.proxy.proxyDepositBase
+  let proxyDepositFactor = client.api.consts.proxy.proxyDepositFactor
+  let proxyDepositTotal = proxyDepositBase.add(proxyDepositFactor.muln(Object.keys(proxyTypes).length))
   assert(proxyDeposit.eq(proxyDepositTotal))
 
   for (const proxy of proxies) {
@@ -84,7 +94,7 @@ export async function addProxyTest<
 
   await client.dev.newBlock()
 
-  await checkEvents(removeProxyEvents, 'proxy').toMatchSnapshot(`events when removing proxies from Alice`)
+  await checkEvents(removeProxyEvents, 'proxy').toMatchSnapshot(`events when removing proxies from Alice (batch)`)
 
   const proxyDataAfterRemoval = await client.api.query.proxy.proxies(alice.address)
   const proxiesAfterRemoval: Vec<PalletProxyProxyDefinition> = proxyDataAfterRemoval[0]
@@ -92,6 +102,58 @@ export async function addProxyTest<
 
   const proxyDepositAfterRemoval = proxyDataAfterRemoval[1]
   assert(proxyDepositAfterRemoval.eq(0))
+
+  // Create proxies (with delay)
+
+  batch = []
+
+  for (const [proxyType, proxyTypeIx] of Object.entries(proxyTypes)) {
+    const addProxyTx = client.api.tx.proxy.addProxy(proxyAccounts[proxyType].address, proxyTypeIx, delay)
+    batch.push(addProxyTx)
+  }
+
+  const batchAddProxyWithDelayTx = client.api.tx.utility.batchAll(batch)
+  // No need to check proxy addition events again - just the delay having changed is uninteresting.
+  await sendTransaction(batchAddProxyWithDelayTx.signAsync(alice))
+
+  await client.dev.newBlock()
+
+  // Check created proxies, again
+
+  proxyData = await client.api.query.proxy.proxies(alice.address)
+  proxies = proxyData[0]
+  assert(proxies.length === Object.keys(proxyTypes).length)
+
+  proxyDeposit = proxyData[1]
+  proxyDepositBase = client.api.consts.proxy.proxyDepositBase
+  proxyDepositFactor = client.api.consts.proxy.proxyDepositFactor
+  proxyDepositTotal = proxyDepositBase.add(proxyDepositFactor.muln(Object.keys(proxyTypes).length))
+  assert(proxyDeposit.eq(proxyDepositTotal))
+
+  for (const proxy of proxies) {
+    await check(proxy)
+      .redact({ removeKeys: /proxyType/ })
+      .toMatchObject({
+        delegate: encodeAddress(proxyAccounts[proxy.proxyType.toString()].address, addressEncoding),
+        delay: delay,
+      })
+  }
+
+  // Remove delay-having proxies
+
+  const removeProxiesTx = client.api.tx.proxy.removeProxies()
+  const removeProxiesEvents = await sendTransaction(removeProxiesTx.signAsync(alice))
+
+  await client.dev.newBlock()
+
+  await checkEvents(removeProxiesEvents, 'proxy').toMatchSnapshot(`events when removing all proxies from Alice`)
+
+  proxyData = await client.api.query.proxy.proxies(alice.address)
+  proxies = proxyData[0]
+  assert(proxies.length === 0)
+
+  proxyDeposit = proxyData[1]
+  assert(proxyDeposit.eq(0))
 }
 
 /**
@@ -111,8 +173,8 @@ export async function proxyE2ETests<
   describe(testConfig.testSuiteName, async () => {
     const [client] = await setupNetworks(chain)
 
-    test('add proxies to an account', async () => {
-      await addProxyTest(client, testConfig.addressEncoding, proxyTypes)
+    test('add proxies (with/without delay) to an account, and remove them', async () => {
+      await addRemoveProxyTest(client, testConfig.addressEncoding, proxyTypes, PROXY_DELAY)
     })
   })
 }
