dd316f98f071f58b19b0f704a1597518d3f1833c
Xiliang Chen <xlchen1291@gmail.com>
2025-05-02 08:53:18 +1200

    add missing snapshot (#245)

--- packages/networks/src/chains/polkadot.ts
+++ packages/networks/src/chains/polkadot.ts
@@ -37,7 +37,7 @@ export const polkadot = defineChain({
 
 export const kusama = defineChain({
   name: 'kusama',
-  endpoint: 'wss://rpc.ibp.network/kusama',
+  endpoint: 'wss://kusama.dotters.network',
   custom: custom.kusama,
   initStorages: getInitStorages(),
   isRelayChain: true,
