badf0e0041f38bac8eaa7a9aecfc003171cb076c
Bryan Chen <xlchen1291@gmail.com>
2025-06-25 09:16:54 +1200

    fix system chains xcm tests

--- packages/polkadot/src/assetHubPolkadot.bridgeHubPolkadot.test.ts
+++ packages/polkadot/src/assetHubPolkadot.bridgeHubPolkadot.test.ts
@@ -4,7 +4,7 @@ import { defaultAccounts } from '@e2e-test/networks'
 import { assetHubPolkadot, bridgeHubPolkadot } from '@e2e-test/networks/chains'
 import { setupNetworks } from '@e2e-test/shared'
 import { query, tx } from '@e2e-test/shared/api'
-import { runXcmPalletDown, runXcmPalletUp } from '@e2e-test/shared/xcm'
+import { runXcmPalletHorizontal } from '@e2e-test/shared/xcm'
 
 describe('assetHubPolkadot & bridgeHubPolkadot', async () => {
   const [assetHubPolkadotClient, bridgeHubPolkadotClient] = await setupNetworks(assetHubPolkadot, bridgeHubPolkadot)
@@ -12,21 +12,23 @@ describe('assetHubPolkadot & bridgeHubPolkadot', async () => {
   const bridgeHubDOT = bridgeHubPolkadot.custom.dot
   const polkadotDOT = assetHubPolkadot.custom.dot
 
-  runXcmPalletDown('assetHubPolkadot transfer DOT to bridgeHubPolkadot', async () => {
+  runXcmPalletHorizontal('assetHubPolkadot transfer DOT to bridgeHubPolkadot', async () => {
     return {
       fromChain: assetHubPolkadotClient,
       toChain: bridgeHubPolkadotClient,
-      balance: query.balances,
+      fromBalance: query.balances,
+      toBalance: query.balances,
       toAccount: defaultAccounts.dave,
       tx: tx.xcmPallet.limitedTeleportAssets(polkadotDOT, 1e12, tx.xcmPallet.parachainV3(1, bridgeHubPolkadot.paraId!)),
     }
   })
 
-  runXcmPalletUp('bridgeHubPolkadot transfer DOT to assetHubPolkadot', async () => {
+  runXcmPalletHorizontal('bridgeHubPolkadot transfer DOT to assetHubPolkadot', async () => {
     return {
       fromChain: bridgeHubPolkadotClient,
       toChain: assetHubPolkadotClient,
-      balance: query.balances,
+      fromBalance: query.balances,
+      toBalance: query.balances,
       toAccount: defaultAccounts.dave,
       tx: tx.xcmPallet.limitedTeleportAssets(
         bridgeHubDOT,
--- packages/polkadot/src/assetHubPolkadot.collectivesPolkadot.test.ts
+++ packages/polkadot/src/assetHubPolkadot.collectivesPolkadot.test.ts
@@ -4,7 +4,7 @@ import { defaultAccounts } from '@e2e-test/networks'
 import { assetHubPolkadot, collectivesPolkadot } from '@e2e-test/networks/chains'
 import { setupNetworks } from '@e2e-test/shared'
 import { query, tx } from '@e2e-test/shared/api'
-import { runXcmPalletDown, runXcmPalletUp } from '@e2e-test/shared/xcm'
+import { runXcmPalletHorizontal } from '@e2e-test/shared/xcm'
 
 describe('assetHubPolkadot & collectivesPolkadot', async () => {
   const [assetHubPolkadotClient, collectivesPolkadotClient] = await setupNetworks(assetHubPolkadot, collectivesPolkadot)
@@ -12,11 +12,12 @@ describe('assetHubPolkadot & collectivesPolkadot', async () => {
   const collectivesDOT = collectivesPolkadot.custom.dot
   const polkadotDOT = assetHubPolkadot.custom.dot
 
-  runXcmPalletDown('assetHubPolkadot transfer DOT to collectivesPolkadot', async () => {
+  runXcmPalletHorizontal('assetHubPolkadot transfer DOT to collectivesPolkadot', async () => {
     return {
       fromChain: assetHubPolkadotClient,
       toChain: collectivesPolkadotClient,
-      balance: query.balances,
+      fromBalance: query.balances,
+      toBalance: query.balances,
       toAccount: defaultAccounts.dave,
       tx: tx.xcmPallet.limitedTeleportAssets(
         polkadotDOT,
@@ -26,11 +27,12 @@ describe('assetHubPolkadot & collectivesPolkadot', async () => {
     }
   })
 
-  runXcmPalletUp('collectivesPolkadot transfer DOT to assetHubPolkadot', async () => {
+  runXcmPalletHorizontal('collectivesPolkadot transfer DOT to assetHubPolkadot', async () => {
     return {
       fromChain: collectivesPolkadotClient,
       toChain: assetHubPolkadotClient,
-      balance: query.balances,
+      fromBalance: query.balances,
+      toBalance: query.balances,
       toAccount: defaultAccounts.dave,
       tx: tx.xcmPallet.limitedTeleportAssets(
         collectivesDOT,
