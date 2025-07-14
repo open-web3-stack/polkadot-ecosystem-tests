8a9ce26cbd8a7e3c757fa461851bddde238154ec
Alexandre Baldé <alexandre.balde@parity.io>
2025-07-14 19:51:59 +0100

    Cast dev accounts objects to`unknown`
    Depdendency mismatch for `@polkadot/keyring` between `chopsticks-utils`
    and `polkadot-ecosystem-tests/networks`.
    Even when `@polkadot/keyring` is set locally (PET) to the same version
    as in `chopsticks-utils`, the dependency mismatch is still present.

--- packages/networks/src/defaultAccounts.ts
+++ packages/networks/src/defaultAccounts.ts
@@ -4,9 +4,7 @@ import type { KeyringInstance, KeyringPair } from '@polkadot/keyring/types'
 
 import { cryptoWaitReady } from '@polkadot/util-crypto'
 
-export type DefaultAccounts = ReturnType<typeof testingPairs>
-
-export const defaultAccounts: {
+export type DefaultAccounts = {
   alice: KeyringPair
   bob: KeyringPair
   charlie: KeyringPair
@@ -19,7 +17,9 @@ export const defaultAccounts: {
   ethan: KeyringPair
   keyring: Keyring
   keyringEth: KeyringInstance
-} = testingPairs()
+}
+
+export const defaultAccounts: DefaultAccounts = testingPairs() as unknown as DefaultAccounts
 
 /**
  * Sr25519 keyring pairs for use in tests.
@@ -27,17 +27,6 @@ export const defaultAccounts: {
  * These are preferrable over Ed25519 because PJS offers Sr25519 development keypairs when used in conjunction with
  * `chopsticks`, which helps debugging tests when `pause()`ing.
  */
-export const defaultAccountsSr25519: {
-  alice: KeyringPair
-  bob: KeyringPair
-  charlie: KeyringPair
-  dave: KeyringPair
-  eve: KeyringPair
-  alith: KeyringPair
-  baltathar: KeyringPair
-  charleth: KeyringPair
-  dorothy: KeyringPair
-  ethan: KeyringPair
-  keyring: Keyring
-  keyringEth: KeyringInstance
-} = await cryptoWaitReady().then(() => testingPairs('sr25519'))
+export const defaultAccountsSr25519: DefaultAccounts = (await cryptoWaitReady().then(() =>
+  testingPairs('sr25519'),
+)) as unknown as DefaultAccounts
