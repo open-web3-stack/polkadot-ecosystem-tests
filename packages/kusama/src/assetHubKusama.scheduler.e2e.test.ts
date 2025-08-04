b2364ebed77a58fb073a4680710f4de820743512
Alexandre R. Baldé <alexandre.balde@parity.io>
2025-08-04 02:44:09 +0100

    Remove AH scheduler e2e tests (#366)
    * Remove unneeded asset hub scheduler e2e tests
    
    * Remove snapshots for AH scheduler tests

--- packages/kusama/src/assetHubKusama.scheduler.e2e.test.ts
+++
@@ -1,4 +0,0 @@
-import { kusama } from '@e2e-test/networks/chains'
-import { schedulerE2ETests } from '@e2e-test/shared'
-
-schedulerE2ETests(kusama, { testSuiteName: 'Asset Hub Kusama Scheduler E2E tests', addressEncoding: 2 })
--- packages/polkadot/src/assetHubPolkadot.scheduler.e2e.test.ts
+++
@@ -1,4 +0,0 @@
-import { polkadot } from '@e2e-test/networks/chains'
-import { schedulerE2ETests } from '@e2e-test/shared'
-
-schedulerE2ETests(polkadot, { testSuiteName: 'Asset Hub Polkadot Scheduler E2E tests', addressEncoding: 0 })
