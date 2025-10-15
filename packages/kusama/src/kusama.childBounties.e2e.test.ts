52fb4c3b3133b4e4a47ebfe6869cc08eb6193c08
Dhiraj Sah <dhiraj@parity.io>
2025-10-15 17:10:32 +0300

    child bounties removed form Kusama relay

--- packages/kusama/src/kusama.childBounties.e2e.test.ts
+++
@@ -1,4 +0,0 @@
-import { kusama } from '@e2e-test/networks/chains'
-import { baseChildBountiesE2ETests, registerTestTree } from '@e2e-test/shared'
-
-registerTestTree(baseChildBountiesE2ETests(kusama, { testSuiteName: 'Kusama Child Bounties', addressEncoding: 2 }))
