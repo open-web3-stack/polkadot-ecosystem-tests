8031d771a0b415a5f88d0435f5e18bf6e4538530
Alexandre Baldé <alexandre.balde@parity.io>
2025-10-23 13:17:33 +0100

    Disable failing Polkadot XCM tests post-AHM

--- packages/polkadot/src/acala.polkadot.test.ts
+++ packages/polkadot/src/acala.polkadot.test.ts
@@ -11,36 +11,48 @@ describe('acala & polkadot', async () => {
   const acalaDOT = acalaClient.config.custom.dot
   const polkadotDOT = polkadotClient.config.custom.dot
 
-  runXtokensUp('acala transfer DOT to polkadot', async () => {
-    return {
-      fromChain: acalaClient,
-      toChain: polkadotClient,
-      balance: query.tokens(acalaDOT),
-      tx: tx.xtokens.transfer(acalaDOT, 1e12, tx.xtokens.relaychainV3),
-    }
-  })
+  runXtokensUp(
+    'acala transfer DOT to polkadot',
+    async () => {
+      return {
+        fromChain: acalaClient,
+        toChain: polkadotClient,
+        balance: query.tokens(acalaDOT),
+        tx: tx.xtokens.transfer(acalaDOT, 1e12, tx.xtokens.relaychainV3),
+      }
+    },
+    { skip: true },
+  )
 
-  runXtokensUp('acala transfer DOT wiht limited weight', async () => {
-    return {
-      fromChain: acalaClient,
-      toChain: polkadotClient,
-      balance: query.tokens(acalaDOT),
-      tx: tx.xtokens.transfer(acalaDOT, 1e12, tx.xtokens.relaychainV3, {
-        Limited: { refTime: 5000000000, proofSize: 10000 },
-      }),
-    }
-  })
+  runXtokensUp(
+    'acala transfer DOT wiht limited weight',
+    async () => {
+      return {
+        fromChain: acalaClient,
+        toChain: polkadotClient,
+        balance: query.tokens(acalaDOT),
+        tx: tx.xtokens.transfer(acalaDOT, 1e12, tx.xtokens.relaychainV3, {
+          Limited: { refTime: 5000000000, proofSize: 10000 },
+        }),
+      }
+    },
+    { skip: true },
+  )
 
-  runXcmPalletDown('polkadot transfer DOT to acala', async () => {
-    return {
-      fromChain: polkadotClient,
-      toChain: acalaClient,
-      balance: query.tokens(acalaDOT),
-      tx: tx.xcmPallet.limitedReserveTransferAssetsV3(
-        polkadotDOT,
-        1e12,
-        tx.xcmPallet.parachainV3(0, acalaClient.config.paraId!),
-      ),
-    }
-  })
+  runXcmPalletDown(
+    'polkadot transfer DOT to acala',
+    async () => {
+      return {
+        fromChain: polkadotClient,
+        toChain: acalaClient,
+        balance: query.tokens(acalaDOT),
+        tx: tx.xcmPallet.limitedReserveTransferAssetsV3(
+          polkadotDOT,
+          1e12,
+          tx.xcmPallet.parachainV3(0, acalaClient.config.paraId!),
+        ),
+      }
+    },
+    { skip: true },
+  )
 })
