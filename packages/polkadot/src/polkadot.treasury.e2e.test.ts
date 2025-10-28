40b55a143f1f37d5dd1930071fa58d448100bb17
Dhiraj Sah <dhiraj@parity.io>
2025-10-28 17:15:10 +0530

    treasury deleted from polkadot relay

--- packages/polkadot/src/polkadot.treasury.e2e.test.ts
+++
@@ -1,6 +0,0 @@
-import { assetHubPolkadot, polkadot } from '@e2e-test/networks/chains'
-import { baseTreasuryE2ETests, registerTestTree } from '@e2e-test/shared'
-
-registerTestTree(
-  baseTreasuryE2ETests(polkadot, assetHubPolkadot, { testSuiteName: 'Polkadot Treasury', addressEncoding: 0 }),
-)
