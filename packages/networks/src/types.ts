9804cfb32eab3e1652abeaba26c23c629bc76c9d
Alexandre Baldé <alexandre.balde@parity.io>
2026-02-19 01:31:42 +0000

    Create/use chain properties type

--- packages/networks/src/types.ts
+++ packages/networks/src/types.ts
@@ -1,13 +1,42 @@
 import type { SetupOption } from '@acala-network/chopsticks-testing'
 
+import type { FeeExtractor } from '@e2e-test/shared'
+
+/**
+ * Chain properties useful during tests; these are made available in the client object, which will have been built
+ * from a chain object containing these properties.
+ */
+export interface ChainProperties {
+  /**
+   * The chain's address encoding; decides SS58 encoding that addresses from that chain follow.
+   * Useful when comparing addresses in tests.
+   */
+  addressEncoding: number
+  /**
+   * Whether the chain uses its own `Local` block numbers to e.g. index its scheduler's agenda, or another block
+   * provider e.g. `NonLocal` means the relay's block number is used.
+   */
+  blockProvider: 'Local' | 'NonLocal'
+  /**
+   * Whether the chain's ED is low relative to the average transaction fee, or of a similar order of magnitude.
+   */
+  chainEd: 'LowEd' | 'Normal'
+  /**
+   * How to query and process a transaction fee payment event from this particular chain (see {@link FeeExtractor}).
+   */
+  feeExtractor?: FeeExtractor
+}
+
 interface ChainConfigRelaychain {
   isRelayChain: true
   paraId?: undefined
+  properties: ChainProperties
 }
 
 interface ChainConfigParachain {
   isRelayChain?: false
   paraId: number
+  properties: ChainProperties & { asyncBacking: 'Enabled' | 'Disabled' }
 }
 
 type ChainConfigBase = {
