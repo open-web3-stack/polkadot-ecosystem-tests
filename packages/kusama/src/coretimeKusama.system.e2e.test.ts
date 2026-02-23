3b2c94ebc58fb943962972a23e2161bfc31eaa72
Alexandre R. Baldé <alexandre.balde@parity.io>
2026-02-23 01:34:59 +0000

    Reenable Kusama relay system tests that required preimage (#527)

--- packages/kusama/src/coretimeKusama.system.e2e.test.ts
+++ packages/kusama/src/coretimeKusama.system.e2e.test.ts
@@ -1,11 +1,10 @@
-import { assetHubKusama, coretimeKusama } from '@e2e-test/networks/chains'
+import { assetHubKusama, coretimeKusama, kusama } from '@e2e-test/networks/chains'
 import { registerTestTree, systemE2ETestsViaRemoteScheduler, type TestConfig } from '@e2e-test/shared'
 
 const testConfig: TestConfig = {
   testSuiteName: 'Kusama Coretime System',
 }
 
-// TODO: Uncomment after Kusama 2.0+ release due to polkadot-fellows/runtimes#957
-// registerTestTree(systemE2ETestsViaRemoteScheduler(kusama, coretimeKusama, testConfig))
+registerTestTree(systemE2ETestsViaRemoteScheduler(kusama, coretimeKusama, testConfig))
 
 registerTestTree(systemE2ETestsViaRemoteScheduler(assetHubKusama, coretimeKusama, testConfig))
