2de597b71e7b97c21a4ede63e6d729b46b3f262e
Karol Kokoszka <karol@parity.io>
2026-03-30 19:33:46 +0200

    Enable `Asset Hub authorizes Polkadot upgrade via Collectives` scenario (#577)
    * Bring back assetHubPolkadot.polkadot.collectivesPolkadot scenario
    
    * Update chopsticks to 1.3.0

--- packages/polkadot/src/assetHubPolkadot.polkadot.collectivesPolkadot.upgrade.e2e.test.ts
+++ packages/polkadot/src/assetHubPolkadot.polkadot.collectivesPolkadot.upgrade.e2e.test.ts
@@ -16,7 +16,7 @@ describe('asset hub & polkadot & collectives', async () => {
     collectivesPolkadot,
   )
 
-  test.skip('Asset Hub authorizes Polkadot upgrade via Collectives', async () => {
+  test('Asset Hub authorizes Polkadot upgrade via Collectives', async () => {
     await authorizeUpgradeViaCollectives(assetHubPolkadotClient, polkadotClient, collectivesClient)
   })
 })
