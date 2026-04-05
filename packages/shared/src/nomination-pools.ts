2d4d3ecde27da9ea2ccc6ab60506e0e8912b6160
Alexandre Baldé <alexandre.balde@parity.io>
2026-04-05 01:57:18 +0000

    Address GH Copilot's comment

--- packages/shared/src/nomination-pools.ts
+++ packages/shared/src/nomination-pools.ts
@@ -1510,8 +1510,8 @@ async function nominationPoolSlashAndWithdrawTest<
   // This avoids BTreeMap encoding issues with setStorage.
   const subPoolsJson = subPools.toJSON() as any
   subPoolsJson.withEra[unbondEraNum.toString()] = {
-    points: Number(originalPoints),
-    balance: Number(slashedBalance),
+    points: '0x' + originalPoints.toString(16),
+    balance: '0x' + slashedBalance.toString(16),
   }
 
   await client.dev.setStorage({
